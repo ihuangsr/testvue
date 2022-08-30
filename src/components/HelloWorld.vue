9794264b0b58e5e1c61a9a0dab787b2c45a713f1
ihsr <334247106@qq.com>
2022-08-30 16:24:21 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,48 @@
 <template>
-  <div class="hello">
-    <h1>fhiodfjodsjfoasehgiohoasdh</h1>
-    <h1>fhiodfjodsjfoasehgiohoasdh</h1>
-    <h1>fhiodfjodsjfoasehgiohoasdh</h1>
-    <h1>fhiodfjodsjfoasehgiohoasdh</h1>
-    <h1>fhiodfjodsjfoasehgiohoasdh</h1>
-  </div>
+  <vxe-table
+      border
+      show-header-overflow
+      show-overflow
+      :row-config="{isHover: true}"
+      :data="tableData2">
+    <!--    <vxe-column type="seq" title="序号" width="60"></vxe-column>-->
+    <vxe-column field="name" title="名人"></vxe-column>
+    <vxe-column field="verticals" title="类目"></vxe-column>
+    <vxe-column field="url" title="预览链接"></vxe-column>
+    <vxe-column field="dec" title="备注"></vxe-column>
+  </vxe-table>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+import {defineComponent, ref} from 'vue'
+
+export default defineComponent({
+  setup() {
+    const tableData2 = ref([
+      {name: 'kelly clarkson', url: 'https://kellyclarkson01.pages.dev/', verticals: 'US Diet', dec: ''},
+    ])
+    return {
+      tableData2
+    }
   }
-}
+})
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
 
-createApp(App).mount('#app')
+function useTable(App) {
+    App.use(VXETable)
+
+    // 给 vue 实例挂载内部对象，例如：
+    // app.config.globalProperties.$XModal = VXETable.modal
+    // app.config.globalProperties.$XPrint = VXETable.print
+    // app.config.globalProperties.$XSaveFile = VXETable.saveFile
+    // app.config.globalProperties.$XReadFile = VXETable.readFile
+}
+
+createApp(App).use(useTable).mount('#app')
