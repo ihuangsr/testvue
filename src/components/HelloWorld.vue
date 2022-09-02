05647f650a59b4d27f92adfa4117b26c32923579
ihsr <334247106@qq.com>
2022-09-02 22:34:41 +0800

    20

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,26 +19,80 @@ import {defineComponent, ref} from 'vue'
 export default defineComponent({
   setup() {
     const tableData2 = ref([
-      {name: 'dite-us-kelly-clarkson-001', url: 'https://kellyclarkson01.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'ed-cbd-us-5pill-002', url: 'https://5pill-ed1.pages.dev/', verticals: 'US Ed', dec: 'ed1' },
-      {name: 'dite-us-0prah-003', url: 'https://0prah-03.pages.dev/', verticals: 'US Diet', dec: 'Diet02' },
-      {name: 'ed-cbd-us-enlarge-004', url: 'https://enlarge-ed02.pages.dev/', verticals: 'US Ed', dec: 'ed02' },
-      {name: 'ed-cbd-us-sexologis-005', url: 'https://ed03.pages.dev/', verticals: 'US Ed', dec: 'ed03' },
-      {name: 'ed-cbd-us-381cm-006', url: 'https://ed04.pages.dev/', verticals: 'US Ed', dec: 'ed04' },
-      {name: 'dite-us-dolly-parton-007', url: 'https://dolly-parton04.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'cbd-us-reba-mcentire-008', url: 'https://reba-mcentire.pages.dev/', verticals: 'US Cbd', dec: '' },
-      {name: 'dite-us-rebel-wilson-009', url: 'https://dite-us-rebel-wilson-002.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-ree-drummond-010', url: 'https://dite-us-ree-drummond-010.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'cbd-us-whoopi-goldberg-011', url: 'https://cbd-us-whoopi-goldberg-011.pages.dev/', verticals: 'US Cbd', dec: '' },
-      {name: 'dite-us-kelly-clarkson-02-012', url: 'https://dite-us-kelly-clarkson-02-012.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-0prah-02-013', url: 'https://dite-us-0prah-02-013.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-miranda-lambert-014', url: 'https://dite-us-miranda-lambert-14.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-shank-tank-015', url: 'https://dite-us-emily-senstrom-015.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-shank-tank-02-016', url: 'https://dite-us-shank-tank-02-016.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-trisha-earwood-017', url: 'https://dite-us-trisha-earwood-017.pages.dev/', verticals: 'US Diet', dec: '' },
-      {name: 'dite-us-kelly-clarkson-03-018', url: 'https://dite-us-kelly-clarkson-03-018.pages.dev/', verticals: 'US Diet', dec: '' },
-	    {name: 'dite-us-ina-garten-019', url: 'https://dite-us-ina-garten-019.pages.dev/', verticals: 'US Diet', dec: '' },
-	    {name: 'dite-us-raba-mcentire-020', url: 'https://dite-us-raba-mcentire-020.pages.dev', verticals: 'US Diet', dec: '' },
+      {name: 'dite-us-kelly-clarkson-001', url: 'https://kellyclarkson01.pages.dev/', verticals: 'US Diet', dec: ''},
+      {name: 'ed-cbd-us-5pill-002', url: 'https://5pill-ed1.pages.dev/', verticals: 'US Ed', dec: 'ed1'},
+      {name: 'dite-us-0prah-003', url: 'https://0prah-03.pages.dev/', verticals: 'US Diet', dec: 'Diet02'},
+      {name: 'ed-cbd-us-enlarge-004', url: 'https://enlarge-ed02.pages.dev/', verticals: 'US Ed', dec: 'ed02'},
+      {name: 'ed-cbd-us-sexologis-005', url: 'https://ed03.pages.dev/', verticals: 'US Ed', dec: 'ed03'},
+      {name: 'ed-cbd-us-381cm-006', url: 'https://ed04.pages.dev/', verticals: 'US Ed', dec: 'ed04'},
+      {name: 'dite-us-dolly-parton-007', url: 'https://dolly-parton04.pages.dev/', verticals: 'US Diet', dec: ''},
+      {name: 'cbd-us-reba-mcentire-008', url: 'https://reba-mcentire.pages.dev/', verticals: 'US Cbd', dec: ''},
+      {
+        name: 'dite-us-rebel-wilson-009',
+        url: 'https://dite-us-rebel-wilson-002.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'dite-us-ree-drummond-010',
+        url: 'https://dite-us-ree-drummond-010.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'cbd-us-whoopi-goldberg-011',
+        url: 'https://cbd-us-whoopi-goldberg-011.pages.dev/',
+        verticals: 'US Cbd',
+        dec: ''
+      },
+      {
+        name: 'dite-us-kelly-clarkson-02-012',
+        url: 'https://dite-us-kelly-clarkson-02-012.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {name: 'dite-us-0prah-02-013', url: 'https://dite-us-0prah-02-013.pages.dev/', verticals: 'US Diet', dec: ''},
+      {
+        name: 'dite-us-miranda-lambert-014',
+        url: 'https://dite-us-miranda-lambert-14.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'dite-us-shank-tank-015',
+        url: 'https://dite-us-emily-senstrom-015.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'dite-us-shank-tank-02-016',
+        url: 'https://dite-us-shank-tank-02-016.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'dite-us-trisha-earwood-017',
+        url: 'https://dite-us-trisha-earwood-017.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'dite-us-kelly-clarkson-03-018',
+        url: 'https://dite-us-kelly-clarkson-03-018.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      }, {
+        name: 'dite-us-ina-garten-019',
+        url: 'https://dite-us-ina-garten-019.pages.dev/',
+        verticals: 'US Diet',
+        dec: ''
+      },
+      {
+        name: 'dite-us-raba-mcentire-020',
+        url: 'https://dite-us-raba-mcentire-020.pages.dev',
+        verticals: 'US Diet',
+        dec: ''
+      },
     ])
     return {
       tableData2
